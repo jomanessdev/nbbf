d1227f5f47cca2a893ecbb70bb22bc3849fe0152
Jordan <jomanessdev@gmail.com>
2017-09-02 00:27:51 -0400

    fixes

--- src/app/views/main/main.component.ts
+++ src/app/views/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { VideoBackground as VideoBackgroundRenderer } from '@squarespace/video-background-rendering';
 
 @Component({
   selector: 'app-main',
@@ -10,6 +11,13 @@ export class MainComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    const config = {
+      container: document.documentElement.querySelector('.sqs-video-background'),
+      url: 'https://youtu.be/qzA8RVE6rzA'/*,
+      useCustomFallbackImage: true*/
+    };
+    const myVideoBackground = new VideoBackgroundRenderer(config);
   }
 
-}
+  }
